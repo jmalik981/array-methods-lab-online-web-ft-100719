9ca0a5248033d0b254fdce32ad9de260b8c2201f
Junaid Malik <jmalik981@gmail.com>
2019-10-21 13:14:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,15 +1,12 @@
 def using_include(array, element)
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
 array.include?(element)
 end
 
 def using_sort(array)
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
 array.sort
 end
 
 def using_reverse(array)
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
 array.reverse
 end
 
